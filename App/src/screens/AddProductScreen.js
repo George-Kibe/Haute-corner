fc03621391a0109504bffcb1d1dad29f888e9cc8
George-Kibe <georgekibewambui@gmail.com>
2023-03-21 21:16:36 +0300

    upload product form UI

--- App/src/screens/AddProductScreen.js
+++ App/src/screens/AddProductScreen.js
@@ -1,11 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import React, {useState} from 'react';
+import {Text, StyleSheet, View, TextInput, TouchableOpacity, ScrollView, Pressable} from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import Ionicons from "react-native-vector-icons/Ionicons"
 const AddProductScreen = () => {
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [category, setCategory] = useState();
+  // const [image, setImage] = useState();
+  const [images, setImages] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [price, setPrice] = useState();
+
+  const goSetOptions = (text) => {
+    const options = text.split(",");
+    setOptions(options)
+  } 
+  // console.log("Options", options)
+  
+
   return (
-    <View style={styles.mainContainer}>
-      <Text style={styles.text}>AddProductScreen</Text>
-    </View>
+    <ScrollView style={styles.mainContainer}>
+      <Text style={styles.title}> Add Product Details</Text>
+      <View style={styles.inputView}>
+        <Text style={styles.text}>Title:</Text>
+        <TextInput value={title} onChangeText={setTitle} style={styles.input} placeholder='Title here' />
+      </View>
+      <View style={styles.inputView}>
+        <Text style={styles.text}>Description:</Text>
+        <TextInput	multiline={true} style={styles.input} 
+          numberOfLines={3}
+          onChangeText={(text) => setDescription(text)}
+          placeholder='Product Description' 
+          value={description}
+        />
+        <Text style={styles.text}>Category:</Text>
+        <Picker
+          selectedValue={category}
+          onValueChange={currentcategory => setCategory(currentcategory)}>
+          <Picker.Item label="Shoes" value="Shoes" />
+          <Picker.Item label="Clothes" value="Clothes" />
+          <Picker.Item label="Men" value="For Men" />
+          <Picker.Item label="Women" value="For Women" />
+          <Picker.Item label="Children" value="For Children" />
+        </Picker>
+        <Text>Selected: {category? category: "None"}</Text>
+      </View>
+      <View style={styles.inputView}>
+        <Text style={styles.text}>Price:</Text>
+        <TextInput value={price} onChangeText={setPrice} keyboardType="number-pad" style={styles.input} placeholder='Price' />
+      </View>
+      <View style={styles.inputView}>
+        <Text style={styles.text}>Options:</Text>
+        <TextInput onChangeText={(text) => goSetOptions(text)} style={styles.input} placeholder='Options Separate Options with ,' />
+      </View>
+      <View style={styles.inputView}>
+        <Text style={styles.text}>Images:</Text>
+        <View>
+          <Pressable><Ionicons name='cloud-upload' size={25} /></Pressable>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.saveButton}>
+        <Text style={styles.saveText}>Save Product</Text>
+      </TouchableOpacity>
+    </ScrollView>
   )
 }
 
@@ -13,11 +70,43 @@ export default AddProductScreen
 
 const styles = StyleSheet.create({
     mainContainer: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
+      padding: "10%",
+    },
+    title: {
+      color: "black",
+      alignSelf: "flex-start",
+      fontWeight: "bold",
+      fontSize: 20,
+      alignSelf: "center",
+      letterSpacing: 1
     },
     text: {
         color: "#000"
     },
+    inputView: {
+      width: "100%",
+      marginTop: 5
+    },
+    input: {
+      backgroundColor: "#e5e7eb",
+      color: "#000",
+      marginTop: 10,
+      borderRadius: 10,
+      padding: 10,
+      fontSize: 15,
+    },
+    saveButton: {
+      marginTop: 70,
+      width: "100%",
+      backgroundColor: "black",
+      height: 40,
+      borderRadius: 20,
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    saveText: {
+      color: "#fff",
+      fontWeight: "bold",
+      fontSize: 18
+    }
 })
--- App/src/screens/ProfileScreen.js
+++ App/src/screens/ProfileScreen.js
@@ -1,13 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Button, Image, StyleSheet, Text } from 'react-native';
+import { launchCamera } from 'react-native-image-picker';
+import { encode } from 'base-64';
 
 const ProfileScreen = () => {
+  const img = "data:image/jpeg;base64,ZmlsZTovLy9kYXRhL3VzZXIvMC9jb20ubmlrZWFwcC9jYWNoZS9ybl9pbWFnZV9waWNrZXJfbGliX3RlbXBfOWZmMGM1YTctYjM4YS00N2Q5LTg1MzYtZDFlZTVhNmJiZGUwLmpwZw=="
+  const [imagesList, setImagesList] = useState([]);
+
+  const handleCapture = () => {
+    launchCamera(
+      {
+        mediaType: 'photo',
+        saveToPhotos: true,
+      },
+      (response) => {
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        } else {
+          const imageData = response.assets[0];
+          const base64 = `data:${imageData.type};base64,${encode(imageData.uri)}`;
+          setImagesList([...imagesList, base64]);
+        }
+      },
+    );
+  };
+  console.log(imagesList)
   return (
-    <View style={styles.mainContainer}>
-      <Text style={styles.text}>AddProductScreen</Text>
+    <View>
+      <Button title="Capture Image" onPress={handleCapture} />
+      <Text>Images</Text>
+      {imagesList.length > 0 && imagesList.map((image, index) => (
+        <Image key={index} source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      ))}
+      <Text>Extra Image</Text>
+      <Image source={img} style={{ width: 200, height: 200 }} />
     </View>
-  )
-}
+    
+  );
+};
 
 export default ProfileScreen;
 
