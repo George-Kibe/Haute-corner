e991edba7dd12e2be01015b21806ce090c787461
George-Kibe <georgekibewambui@gmail.com>
2024-08-22 17:27:47 +0300

    updating bundle

--- HauteCorner/android/app/src/main/java/com/hautecorner/app/MainActivity.kt
+++ HauteCorner/android/app/src/main/java/com/hautecorner/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hautecorner
+package com.hautecorner.app
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- HauteCorner/android/app/src/main/java/com/hautecorner/app/MainApplication.kt
+++ HauteCorner/android/app/src/main/java/com/hautecorner/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.hautecorner
+package com.hautecorner.app
 
 import android.app.Application
 import com.facebook.react.PackageList
